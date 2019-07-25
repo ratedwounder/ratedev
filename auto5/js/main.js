d4a72bf2203ca5ec7f5148c0f3a39f3ecb1986ff
OlegZimin <pm.ziminoleg@gmail.com>
2019-07-25 17:02:24 +0300

    + adaptive
    +mob

--- auto5/js/main.js
+++ auto5/js/main.js
@@ -3,3 +3,14 @@ $(document).ready(function() {
         console.log('callback - particles.js config loaded');
     });
 });
+
+$(document).ready(function () {
+    $("#list").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({
+            scrollTop: top
+        }, 1000);
+    });
+});
